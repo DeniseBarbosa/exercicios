99419a6ef5ba69f3291416e6ddb7a5e7049404dd
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-11 15:32:20 -0300

    condicional if

---
+++ if.js
@@ -0,0 +1,9 @@
+const idade = 18;
+
+if ( idade >= 18) {
+    //é executado caso a condição seja verdadeira
+    console.log("É maior de idade");
+}else{
+    console.log("Menor de idade");
+}
+console.log("Fim do codigo");

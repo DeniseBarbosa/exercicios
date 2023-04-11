7ff7f2d945e716b272b0c7e1262e7da8c2d780a8
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-11 15:22:11 -0300

    Operadores de identidade

---
+++ operadores-identidade.js
@@ -0,0 +1,12 @@
+// 18 é identico a 18?
+// além de ter o mesmo conteudo tem o mesmo tipo
+const identico = 18 ===18;
+console.log(identico);
+const identico1 = "denise" === 17;
+console.log(identico1);
+const identico2 = "denise" === "denise";
+console.log(identico2);
+const identico3 = "dog" == "cat";
+console.log(identico3);
+const identico4 = "dog" == "dog";
+console.log(identico4);

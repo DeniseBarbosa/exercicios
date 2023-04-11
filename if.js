3522fe700cbaa49a1aaf1e52ee8d2d3b4eb26b29
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-11 15:47:26 -0300

    if else

--- if.js
+++ if.js
@@ -1,9 +1,10 @@
-const idade = 18;
+const idade = 9;
 
 if ( idade >= 18) {
     //é executado caso a condição seja verdadeira
     console.log("É maior de idade");
 }else{
+    //caso a condição seja falso é executado
     console.log("Menor de idade");
 }
 console.log("Fim do codigo");

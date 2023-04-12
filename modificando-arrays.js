273e2dcb0959a2af984909639ba697929a6b663e
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-12 14:46:37 -0300

    manipulando array

---
+++ modificando-arrays.js
@@ -0,0 +1,6 @@
+const nomesDeAlunos = ['amanda', 'pedro','jordania','marcio','felipe'];
+nomesDeAlunos[2] = 'modificado';
+nomesDeAlunos[0] = 'amanda silva';
+const x = 4;
+nomesDeAlunos[x] = 'renato';
+console.log(nomesDeAlunos);

eed1ff18eddc98e3075e942e7feaf0b971b48e25
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-12 14:53:31 -0300

    tamanho array

---
+++ tamanho-array.js
@@ -0,0 +1,5 @@
+const nomesDeAlunos = ['amanda', 'pedro','jordania','marcio','felipe','julio','paulo','luana','renata'];
+// adicionando um array no final da fila
+nomesDeAlunos[nomesDeAlunos.length] = 'final da fila';
+// const x = 
+console.log(nomesDeAlunos);

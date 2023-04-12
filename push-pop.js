06c34f6fa55cf8da1518d80196ad5b73a675b74e
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-12 14:59:33 -0300

    push pop array

---
+++ push-pop.js
@@ -0,0 +1,6 @@
+const nomeAnimais = ['cobra','coelho','vaca','carneiro','zebra'];
+// colocar no final, adiciona no final
+nomeAnimais.push('serpente');
+// desfazer a adição, remove o que tiver no final
+nomeAnimais.pop();
+console.log(nomeAnimais);

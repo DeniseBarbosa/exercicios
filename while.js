9e2ef32cbf56dbbb180d88bff377ff0e3d1095b4
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-12 15:45:13 -0300

    while

---
+++ while.js
@@ -0,0 +1,9 @@
+const nomesDeFuncionarios = ['luanda','marcio','fernanda','cintia','jorge','laura'];
+
+let indice = 0
+while (indice < 5  ) {
+    console.log(nomesDeFuncionarios[indice]);
+    indice++
+}
+
+

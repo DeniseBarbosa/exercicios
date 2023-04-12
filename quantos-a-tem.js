58e860051e0b2babfe218d1b140ce7472b02f228
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-12 17:14:32 -0300

    exercícios

---
+++ quantos-a-tem.js
@@ -0,0 +1,23 @@
+const palavra = 'Casa da arvoreaaaa';
+
+let quantidade = 0;
+for(let letra of palavra){
+    if(letra === 'a'){
+        quantidade++;
+    }
+}
+//console.log(quantidade);
+
+for (let i = 10; i >= 0; i--){
+   // console.log(i);
+}
+
+let numero = 2;
+while( numero <= 10){
+   // console.log(numero);
+    numero = numero + 2;
+}
+
+for (let numeroPar = 2; numeroPar <=10; numeroPar = numeroPar + 2) {
+    console.log(numeroPar);
+}

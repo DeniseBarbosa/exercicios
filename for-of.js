d862773cbaf41165cce69ce59018d5d0806e5f53
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-12 16:37:51 -0300

    (for of ) forma simples de percorrer um array

---
+++ for-of.js
@@ -0,0 +1,6 @@
+// forma mais pratica de se percorrer uma array usando o javaScript
+
+const nomes = ['amanda','paulo','pedro','guilherme','cintia'];
+for(let nome of nomes){
+    console.log(nome);
+}

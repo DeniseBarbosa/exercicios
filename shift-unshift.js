7102abb36bcecf040684a45a46d1a341f5a28c23
DeniseBarbosa <deniseluvoir@gmail.com>
2023-04-12 15:09:38 -0300

    shift unshift

---
+++ shift-unshift.js
@@ -0,0 +1,6 @@
+const nomeDeCidades = ['são paulo','manaus','bahia','guarulhos'];
+// remove o primeiro elemento
+nomeDeCidades.shift();
+//inserir o elemento no começo do array
+nomeDeCidades.unshift('para')
+console.log(nomeDeCidades);
